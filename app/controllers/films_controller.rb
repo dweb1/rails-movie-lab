bb966c2a74dc679a8fbae02ac21b76ee711d9f44
dweb1 <david.p.weber@gmail.com>
2017-08-29 15:37:42 -0400

    New movie submit path working

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -6,4 +6,18 @@ class FilmsController < ApplicationController
   def show
     @film = Film.find(params[:id])
   end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.create(film_params)
+    redirect_to film_path(@film)
+  end
+
+  private
+  def film_params
+    params.require(:film).permit(:title, :year, :synopsis, :picture, :genre)
+  end
 end
