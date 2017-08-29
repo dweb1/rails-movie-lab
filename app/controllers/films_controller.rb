42852940338d28c86869529840d4fc9718263143
dweb1 <david.p.weber@gmail.com>
2017-08-29 15:44:32 -0400

    Edit route working for film

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -16,6 +16,16 @@ class FilmsController < ApplicationController
     redirect_to film_path(@film)
   end
 
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  def update
+    @film = Film.find(params[:id])
+    @film.update(film_params)
+    redirect_to film_path(@film)
+  end
+
   private
   def film_params
     params.require(:film).permit(:title, :year, :synopsis, :picture, :genre)
